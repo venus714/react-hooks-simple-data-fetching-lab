dd9a356ad0ce39e16ed0baccb08d300ed2398180
Student <venus.kabera@student.moringaschool.com>
2023-01-14 23:53:39 +0300

    Done

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,22 @@
 // create your App component here
+import React, {useEffect,useState} from "react";
+
+const API ='https://dog.ceo/api/breeds/image/random'
+function  DogImage() {
+    const[image , setImage] = useState(null) 
+    useEffect(()=>{
+        fetch (API)
+        .then(res=>res.json())
+        .then(data=>setImage(data.message))
+
+    },[])
+    return(
+    <div>
+        <p>loading...</p>
+        <img src={image} alt="A Random Dog"/>
+
+    </div>
+    );
+}
+export default DogImage
+
